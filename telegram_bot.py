ed59890bb447bbb8099d8b173d1e869746a522a6
annaskvortsova <annata.skvortsova@gmail.com>
2023-06-01 09:53:47 +0300

    fix6.7

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,4 @@
 import os
-
 import requests
 
 # Получите токен вашего телеграм-бота
@@ -8,10 +7,7 @@ url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
 
 
 def send_message():
-    telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
-
-    chat_id = 808945403
-
+    chat_id = 808945403  # Замените на ваш chat_id
     total_tests = os.environ.get('TOTAL_TESTS', 'N/A')
     passed_tests = os.environ.get('PASSED_TESTS', 'N/A')
     failed_tests = os.environ.get('FAILED_TESTS', 'N/A')
@@ -33,3 +29,6 @@ def send_message():
         print("Сообщение успешно отправлено")
     else:
         print("Ошибка при отправке сообщения:", response.status_code)
+
+
+send_message()
