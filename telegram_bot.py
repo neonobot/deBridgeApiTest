a544c063c268a38aab4a7d6abeca3e20c1f3de42
annaskvortsova <annata.skvortsova@gmail.com>
2023-06-02 13:51:58 +0300

    fix9.1

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,41 @@
 import os
+
 import requests
 
-# Получите токен вашего телеграм-бота
+
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
 
 
 def send_message():
-    chat_id = -943316058  # Замените на ваш chat_id
+    chat_id = 808945403
+    total_tests = os.environ.get('TOTAL_TESTS', 'N/A')
+    passed_tests = os.environ.get('PASSED_TESTS', 'N/A')
+    failed_tests = os.environ.get('FAILED_TESTS', 'N/A')
+    skipped_tests = os.environ.get('SKIPPED_TESTS', 'N/A')
+    allure_report = os.environ.get('Allure Report', 'N/A')
+
+    message = f"Результаты выполнения тестов:\n" \
+              f"Всего тестов: {total_tests}\n" \
+              f"Пройдено: {passed_tests}\n" \
+              f"Провалено: {failed_tests}\n" \
+              f"Пропущено: {skipped_tests}\n" \
+              f"Отчет: {allure_report}"
+
+    payload = {
+        "chat_id": chat_id,
+        "text": message
+    }
+
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        print("Сообщение успешно отправлено")
+    else:
+        print("Ошибка при отправке сообщения:", response.status_code)
+
+
+def send_message_to_group():
+    chat_id = -943316058
     total_tests = os.environ.get('TOTAL_TESTS', 'N/A')
     passed_tests = os.environ.get('PASSED_TESTS', 'N/A')
     failed_tests = os.environ.get('FAILED_TESTS', 'N/A')
@@ -34,3 +62,4 @@ def send_message():
 
 
 send_message()
+send_message_to_group()
