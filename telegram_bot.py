abc25a2b9ac3a76b94d9521f413e79e81aa65d8a
annaskvortsova <annata.skvortsova@gmail.com>
2023-06-02 13:46:41 +0300

    fix9

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,4 @@
 import os
-
 import requests
 
 # Получите токен вашего телеграм-бота
@@ -8,7 +7,7 @@ url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
 
 
 def send_message():
-    chat_id = 808945403  # Замените на ваш chat_id
+    chat_id = -943316058  # Замените на ваш chat_id
     total_tests = os.environ.get('TOTAL_TESTS', 'N/A')
     passed_tests = os.environ.get('PASSED_TESTS', 'N/A')
     failed_tests = os.environ.get('FAILED_TESTS', 'N/A')
